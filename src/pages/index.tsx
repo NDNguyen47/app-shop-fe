02df74da9e8d73b83044cad55bcdceb572e2de96
NDNguyen47 <ngductuan472004@gmail.com>
2024-06-10 15:29:22 +0700

    New

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,27 @@
 'use client'
 import Head from 'next/head'
 import Button from '@mui/material/Button'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  const fetchApiUser = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3001/api/users?limit=10&page=1"
+      );
+      
+    } catch (error) {
+      console.error("Error fetching users:", error);
+     
+    }
+  };
+
+  useEffect(() =>{
+    fetchApiUser()
+  } , [])
+
   return (
     <>
       <Head>

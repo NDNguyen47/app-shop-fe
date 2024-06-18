609857bf54566e5150aaaad9c6a11f8aa327015b
NDNguyen47 <ngductuan472004@gmail.com>
2024-06-18 20:12:44 +0700

    update-ui-login-register

---
+++ src/pages/register.tsx
@@ -0,0 +1,15 @@
+
+import { NextPage } from 'next'
+import RegisterPage from 'src/view/pages/register'
+
+type Tprops = {}
+
+const Register: NextPage<Tprops> = () => {
+    return (
+        <RegisterPage/>
+    )
+}
+
+export default Register
+
+
